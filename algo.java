161d5e49d69b0e6c905f181aa5ad32b6facc9c1f
Antony <91181127+Getsugo@users.noreply.github.com>
2021-10-20 15:29:34 +0200

    oui

---
+++ algo.java
@@ -0,0 +1,44 @@
+package ALGOGIT;
+
+public class algo {
+    class  ApplicationTriBulle  
+{
+  static  int [] table  =  new  int [100]  ;  // le tableau a trier en attribut
+  
+   static void  TriBulle  ( )    {
+    int  n  =  table.length - 1 ;
+   for (  int  i  =  n ;  i >= 1 ;  i -- )
+    for (  int  j  =  2 ;  j <= i ;  j ++ ) 
+     if ( table[j - 1]  >  table[j] )
+     {
+       int  temp  =  table[j - 1] ;
+       table[j - 1]  =  table[j] ;
+       table[j]  =  temp ;
+     }
+  }
+  
+  static void  Impression  ( )   {
+    // Affichage du tableau 
+    int  n  =  table.length - 1 ;
+   for (  int  i  =  1 ;  i <= n ;  i ++ ) 
+     System.out.print ( table[i] + " , ");
+    System.out.println ();
+  }
+  
+  static void  Initialisation   ( )    {
+    // remplissage aléatoire du tableau 
+    int  n  =  table.length - 1 ;
+   for (  int  i  =  1 ;  i <= n ;  i ++ ) 
+     table[i]  =  ( int )( Math.random () * 200 );
+  }
+  
+  public static void  main ( String [ ] args )    {
+    Initialisation  ( );
+    System.out.println ("Tableau initial :");
+    Impression  ( );
+    TriBulle  ( );
+    System.out.println ("Tableau une fois trié :");
+    Impression  ( );
+  } 
+ }
+}
