933e07c2eea49ab4579f76e668ed09e32d04e08d
Antony <91181127+Getsugo@users.noreply.github.com>
2021-10-21 11:43:46 +0200

    re

--- algo.java
+++ algo.java
@@ -5,9 +5,10 @@ public class algo {
 {
   static  int [] table  =  new  int [10]  ;  // le tableau a trier en attribut
   
-   static void  TriBulle  ( )    {
-    int  l  =  table.length - 1 ;
-   for (  int  i  =  l ;  i >= 1 ;  i -- )
+  static void TriBulle() {
+
+    int  f  =  table.length - 1 ;
+   for (  int  i  =  f ;  i >= 1 ;  i -- )
     for (  int  j  =  2 ;  j <= i ;  j ++ ) 
      if ( table[j - 1]  >  table[j] )
      {
@@ -15,30 +16,27 @@ public class algo {
        table[j - 1]  =  table[j] ;
        table[j]  =  temp ;
      }
+
   }
-  
-  static void  Impression  ( )   {
-    // Affichage du tableau 
-    int  l  =  table.length - 1 ;
-   for (  int  i  =  1 ;  i <= l ;  i ++ ) 
-     System.out.print ( table[i] + " , ");
-    System.out.println ();
-  }
-  
-  static void  Initialisation   ( )    {
-    // remplie le tableau avec des chiffres aléatoire  
-    int  l  =  table.length - 1 ;
-   for (  int  i  =  1 ;  i <= l ;  i ++ ) 
-     table[i]  =  ( int )( Math.random () * 15 );
-  }
-  
-  public static void  main ( String [ ] args )    {
-    Initialisation  ( );
-    System.out.println ("Tableau non trié :");
-    Impression  ( );
-    TriBulle  ( );
-    System.out.println ("Tableau une fois trié :");
-    Impression  ( );
+ 
+public static void  main ( String [ ] args )    {
+  int [] table = {12,1,4,456,76,34};
+
+  System.out.println ("Tableau non trié :");
+
+  int  l  =  table.length - 1 ;
+  for (  int  i  =  1 ;  i <= l ;  i ++ ) 
+  System.out.print ( table[i] + " , ");
+  System.out.println ();
+
+  TriBulle();
+
+  System.out.println ("Tableau une fois trié :");
+     
+  int  m  =  table.length - 1 ;
+  for (  int  i  =  1 ;  i <= m ;  i ++ ) 
+  System.out.print ( table[i] + " , ");
+  System.out.println ();
   } 
  }
 }
