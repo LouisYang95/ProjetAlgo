9b7377c1d9bd96cd7f42926fe78569e21dd29787
Massimiliano <massimiliano.hubert-abbate@edu.esiee-it.fr>
2021-10-21 14:32:31 +0200

    m

--- Insertion.java
+++ Insertion.java
@@ -1,43 +1,43 @@
 public class Insertion {
      
-    public static void tri_insertion(int tab[]) 
+    public static void tri_insertion(int T[]) 
     {  
-         int taille = tab.length;  
+         int n = T.length;  
        
-         for (int i = 1; i < taille; i++)
+         for (int i = 1; i < n; i++)
          { 
-              int index = tab[i];  
+              int m = T[i];  
               int j = i-1;  
            
-              while(j >= 0 && tab[j] > index)  
+              while(j >= 0 && T[j] > m)  
               {
-                   tab[j+1] = tab[j];  
+                   T[j+1] = T[j];  
                    j--;  
               }  
-              tab[j+1] = index; 
+              T[j+1] = m; 
        }  
     }
- 
-    static void displayTab(int[] tab)
-    {      
+    public static void main(String str[])
+    {   
+         int[] tab = {1,12,4,5,93,21,8,11};  
+       
+         System.out.println("Avant le tri par insertion"); 
+
          for(int i=0; i < tab.length; i++)
          {  
               System.out.print(tab[i] + " ");  
          } 
          System.out.println();
-    }
- 
-    public static void main(String str[])
-    {   
-         int[] tab = {1,12,4,5,93,21,8,11};  
-       
-         System.out.println("Avant le tri par insertion");    
-         displayTab(tab);
            
          //trie un tableau en utilisant le tri par insertion
          tri_insertion(tab); 
           
-         System.out.println("Apres le tri par insertion");  
-         displayTab(tab);   
+         System.out.println("Apres le tri par insertion");
+         
+         for(int i=0; i < tab.length; i++)
+         {  
+              System.out.print(tab[i] + " ");  
+         } 
+         System.out.println();
    } 
 }
