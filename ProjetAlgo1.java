d469ae327232cc1aed142476b856170265c74ffc
LouisYang95 <91289384+LouisYang95@users.noreply.github.com>
2021-10-21 10:18:16 +0200

    ProjetAlgo1.java

---
+++ ProjetAlgo1.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+
+class   ProjetAlgo1   
+{
+ 
+
+  //--------------- AFFICHER LE TABLEAU GRACE A ARRAY -----------------------//
+ static void AfficheTableau  (int [] tab){
+        System.out.println(Arrays.toString(tab));
+}
+ 
+
+ //--------------- CODE POUR LA TRI SELECTION -------------------// 
+
+ static void  TriSelect  (int [] tab)
+ {
+    int l  =  tab.length - 1 ;
+  for (  int  i  =  1 ;  i  <=  l - 1 ;  i++)
+  {                                                     
+    int  m  =  i ;                                      
+    for (  int  j  =  i + 1 ;  j  <=  l ;  j++)       
+     if ( tab[j]  <  tab[m] )                   
+        m  =  j ;                                       
+  
+    int  temp  =  tab[ m ];
+    tab[ m ]  =  tab[ i ];
+    tab[ i ] =  temp;              
+  } 
+
+ }
+ 
+ 
+//--------------- RUN DU CODE ----------------//
+ public static void  main ( String [] args ) 
+ {     int [] tab = {1,2,45,66,777,2326,9};      //VALEUR A METTRE DANS LE TABLEAU
+   System.out.println ("Tableau initial :");
+   AfficheTableau(tab);
+   TriSelect (tab);
+   System.out.println ("Tableau une fois trié :");
+   AfficheTableau  (tab);
+ }  
+}
