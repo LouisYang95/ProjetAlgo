d016d9239052cd084a67bbbff6d21a5a5a4eec91
LouisYang95 <91289384+LouisYang95@users.noreply.github.com>
2021-10-22 09:58:16 +0200

    o

---
+++ Insertion /Insertion.java
@@ -0,0 +1,53 @@
+public class Insertion {
+     
+    public static void tri_insertion(int T[]) 
+    {  
+         int comparaison = 0;
+         int affectation = 0;
+         int echange = 0;
+         int n = T.length;  
+       
+         for (int i = 1; i < n; i++)
+         { 
+               comparaison ++;
+              int m = T[i];  
+              int j = i-1;  
+              affectation +=2;
+           
+              while(j >= 0 && T[j] > m)  
+              {
+               comparaison ++;
+                   T[j+1] = T[j];
+                   affectation ++;  
+                   echange ++;
+                   j--;  
+              }  
+              T[j+1] = m; 
+              affectation ++;
+       }  
+
+       //afficher le tableau trié
+       for(int i=0; i < T.length; i++)
+         {  
+              System.out.print(T[i] + " ");  
+         } 
+         System.out.println();
+         System.out.println("Il y a eu " + comparaison + " comparaisons, " + affectation +" affectations et "+ echange + " echanges.");
+
+    }
+
+    public static void main(String str[])
+    {   
+          //int[] tab = {10,9,8,7,6,5,4,3,2,1};
+
+          int[] tab = {5, 4, 3, 2, 1, 10, 9, 8, 7, 6 };
+
+          // int[] tab = {1,2,3,4,5,6,7,8,9,10};
+          // int[] tab = {1,12,4,5,93,21,8,11};  
+       
+     System.out.println("Apres le tri par insertion"); 
+         tri_insertion(tab); 
+         
+     
+   } 
+}
