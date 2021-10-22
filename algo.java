4dbec4f58824ca91e3b4526a6138fc08ad83862d
Antony <91181127+Getsugo@users.noreply.github.com>
2021-10-22 10:44:02 +0200

    newalgo

--- algo.java
+++ algo.java
@@ -1,42 +1,56 @@
 package ProjetAlgo;
 
-public class algo {
-    class  ApplicationTriBulle  
+public class algo 
 {
-  static  int [] table  =  new  int [10]  ;  // le tableau a trier en attribut
+    class  ApplicationTriBulle  
+    {
   
-  static void TriBulle() {
+      static void  TriBulle  (int table[])    
+      {
+        int affectation = 0;
+        int comparaison = 0;
+        int echange = 0;
 
-    int  f  =  table.length - 1 ;
-   for (  int  i  =  f ;  i >= 1 ;  i -- )
-    for (  int  j  =  2 ;  j <= i ;  j ++ ) 
-     if ( table[j - 1]  >  table[j] )
-     {
-       int  temp  =  table[j - 1] ;
-       table[j - 1]  =  table[j] ;
-       table[j]  =  temp ;
-     }
+        int  l  =  table.length - 1 ;
+       for (  int  i  =  l ;  i >= 1 ;  i -- ){
+       affectation += 1;
+       comparaison += 1;
+        for (  int  j  =  2 ;  j <= i ;  j ++ ) 
+         if ( table[j - 1]  >  table[j] )
+         {
+           int  temp  =  table[j - 1] ;
+           table[j - 1]  =  table[j] ;
+           table[j]  =  temp ;
 
-  }
- 
-public static void  main ( String [ ] args )    {
-  int [] table = {12,1,4,456,76,34};
+           affectation += 2;
+           comparaison += 3;
+           echange += 1;
+         }
+         }
+         System.out.println("Affectations : " + affectation);
+         System.out.println("Comparaisons : " + comparaison);
+         System.out.println("Echanges : " + echange);
+      }
+      static void AfficheTableau (int[] table){
+      int  l  =  table.length - 1;
+      for (  int  i  =  0 ;  i <= l ;  i ++ ) 
+      System.out.print ( table[i] + " , ");
+      System.out.println ();
+      }
 
-  System.out.println ("Tableau non trié :");
+public static void  main ( String [ ] args ) 
+  {
+      int [] table = {1,9,8,7,6,5,4,3,2,10};
 
-  int  l  =  table.length - 1 ;
-  for (  int  i  =  1 ;  i <= l ;  i ++ ) 
-  System.out.print ( table[i] + " , ");
-  System.out.println ();
+      System.out.println ("Tableau non trié :");
 
-  TriBulle();
+      AfficheTableau(table);
 
-  System.out.println ("Tableau une fois trié :");
+      TriBulle(table);
+
+      System.out.println ("Tableau une fois trié :");
      
-  int  m  =  table.length - 1 ;
-  for (  int  i  =  1 ;  i <= m ;  i ++ ) 
-  System.out.print ( table[i] + " , ");
-  System.out.println ();
+      AfficheTableau(table);
   } 
- }
+}
 }
