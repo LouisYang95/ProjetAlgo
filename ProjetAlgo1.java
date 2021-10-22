30e37d90fe55c13b0690c6ed013fbc070c2f4500
LouisYang95 <91289384+LouisYang95@users.noreply.github.com>
2021-10-22 09:31:56 +0200

    projetalgo

--- ProjetAlgo1.java
+++ ProjetAlgo1.java
@@ -14,34 +14,41 @@ class   ProjetAlgo1
 
  static void  TriSelect  (int [] tab)
  {
-    int comparaison = 0;
-    int echange = 0;
-    int affectation =0;
-    int l  =  tab.length - 1 ;
-  for (  int  i  =  1 ;  i  <=  l - 1 ;  i++)
+    int comparaison = 0;        //Lorsqu'il y a comparaison entre 2 valeurs
+    int echange = 0;            //Lorsqu'il y a 3 affectations
+    int affectation =0;         //Lorsqu'on attribut une valeur
+    int l  =  tab.length-1 ;
+
+  for (  int  i  =  1 ;  i  <= l ;  i++)
   {
-      comparaison +=1;                                                     
+        comparaison +=1;  
+        affectation +=1;
+
     int  m  =  i ;                                      
-    for (  int  j  =  i + 1 ;  j  <=  l ;  j++)  
+    for (  int  j  =  i + 1 ;  j  <= l ;  j++)  
      if ( tab[j]  <  tab[m] )                   
-        m  =  j ;                                       
-      comparaison +=1;
-      affectation +=1;
+        m  =  j ;
+
+        comparaison +=1;
+        affectation +=1;
+
     int  temp  =  tab[ m ];
-    tab[ m ]  =  tab[ i ];
-    tab[ i ] =  temp;
-      affectation +=2;
-      echange +=1;
+      tab[ m ]  =  tab[ i ];
+      tab[ i ] =  temp;
+        affectation +=3;
+        echange +=1;
   } 
-  System.out.println (affectation);
-  System.out.println (comparaison);
-  System.out.println (echange);
+  int operation = comparaison + affectation;
+  System.out.println ("Il y a eu : " + affectation + " affections");
+  System.out.println ("Il y a eu : " + comparaison + " comparaisons");
+  System.out.println ("Il y a eu : " + echange + " echanges");
+  System.out.println ("Il y a eu : " + operation + " operations");
  }
  
  
 //--------------- RUN DU CODE ----------------//
  public static void  main ( String [] args ) 
- {     int [] tab = {1,2,45,66,777,2326,9};      //VALEUR A METTRE DANS LE TABLEAU
+ {     int [] tab = {1,1231,65,54,3,4124,421,5435,7657,223};      //VALEUR A METTRE DANS LE TABLEAU
    System.out.println ("Tableau sans le tri:");
    AfficheTableau(tab);
    TriSelect (tab);
